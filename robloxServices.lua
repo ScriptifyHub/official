431280c3a56e18bc6a251c0c3f4223ae2476796b
Scriptify Hub <63516612+ScriptifyHub@users.noreply.github.com>
2026-03-04 06:14:50 -0500

    Update robloxServices.lua

--- robloxServices.lua
+++ robloxServices.lua
@@ -3,14 +3,16 @@ local function returnServices(g)
   game = g
 end
 
+
+local p = game:GetService("Players")
 return {
   RbxAnalyticsService = game:GetService("RbxAnalyticsService"),
   ReplicatedStorage = game:GetService("ReplicatedStorage"),
   HttpService = game:GetService("HttpService"),
   VirtualUser = game:GetService("VirtualUser"),
   UIS = game:GetService("UserInputService"),
-  Players = game:GetService("Players"),
   Camera = workspace.CurrentCamera,
-  Player = Players.LocalPlayer,
+  Players = game:GetService("Players")
+  Player = p.LocalPlayer,
   bv, bg
 }
