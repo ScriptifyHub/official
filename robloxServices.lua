70142d5b11a1b1ac5c20db36d23d83abd5063fff
Scriptify Hub <63516612+ScriptifyHub@users.noreply.github.com>
2026-03-04 06:06:57 -0500

    Create robloxServices.lua

---
+++ robloxServices.lua
@@ -0,0 +1,11 @@
+return {
+  RbxAnalyticsService = game:GetService("RbxAnalyticsService"),
+  ReplicatedStorage = game:GetService("ReplicatedStorage"),
+  HttpService = game:GetService("HttpService"),
+  VirtualUser = game:GetService("VirtualUser"),
+  UIS = game:GetService("UserInputService"),
+  Players = game:GetService("Players"),
+  Camera = workspace.CurrentCamera,
+  Player = Players.LocalPlayer,
+  bv, bg
+}
